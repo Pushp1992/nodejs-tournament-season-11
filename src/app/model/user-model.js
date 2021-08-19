53c48ddeaa04dfbd3426565665921422bcf6343c
Pushp Singh <pushp_singh@condenast.com>
2021-08-19 14:26:06 +0530

    Boiler Plate setup Travel Planner App

---
+++ src/app/model/user-model.js
@@ -0,0 +1,15 @@
+// User List model which expose the schema of user with it's respective fields
+
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    UserId: Number,
+    userName: String,
+    userEmail: String,
+    city: String,
+    travelHobbies: Array
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('UserList', UserSchema);
